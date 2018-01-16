fd39b4c46983fff7787f2bef750e1a86548c3274
Kevin Spaeth <kevin.spaeth93@gmail.com>
2018-01-16 09:57:44 -0600

    Added interval

---
+++ BardMusic/Constants/Interval.cs
@@ -0,0 +1,9 @@
+﻿namespace Constants
+{
+    public class Interval
+    {
+        public const int MINIMUM = 165;
+        public const int RECOMMENDED = 175;
+        public const int MAXIMUM = 185;
+    }
+}
